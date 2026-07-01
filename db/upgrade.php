f1b7a3158f19d5ea7559259dee0d9db4e9692727
Luuk Verhoeven <developer@ldesignmedia.nl>
2026-07-01 12:51:40 +0200

    fix(db): drop empty-string default on previewtext CHAR NOT NULL
    XMLDB forbids a '' default on a CHAR NOT NULL column and emits a
    developer debug warning that becomes fatal during moodle-plugin-ci
    test install (CI red across all Moodle versions on main).
    
    Remove DEFAULT='' from local_mailwhistle_templates.previewtext in
    install.xml and drop the '' default arg in the db/upgrade.php create
    step, matching the sibling name/subject fields. Bump version to
    2026070110.

--- db/upgrade.php
+++ db/upgrade.php
@@ -44,7 +44,7 @@ function xmldb_local_mailwhistle_upgrade(int $oldversion): bool {
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
         $table->add_field('subject', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('previewtext', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, '');
+        $table->add_field('previewtext', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
         $table->add_field('bodyhtml', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('bodytext', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
