6b71b87aa0ade097673639ae8e2c850d4c692433
Luuk Verhoeven <developer@ldesignmedia.nl>
2026-07-03 13:12:29 +0200

    fix: add docblock to entity get_available_columns()
    Removing the #[\Override] attribute in the previous commit left
    get_available_columns() with no docblock, which the codechecker requires
    (moodle.Commenting.MissingDocblock.Function). Add a proper PHPDoc block that
    also documents why the #[\Override] attribute is intentionally omitted.
    
    Bump version to 2026070122 (1.2.9).

--- classes/reportbuilder/local/entities/campaign.php
+++ classes/reportbuilder/local/entities/campaign.php
@@ -69,9 +69,15 @@ class campaign extends base {
         return $this;
     }
 
-    // No #[\Override]: get_available_columns() is not a parent method on the
-    // Moodle 5.0/5.1 base entity (it was formalised in 5.2), so the attribute
-    // would fatal on those branches.
+    /**
+     * Return the columns available for this entity.
+     *
+     * No #[\Override] attribute is used here: get_available_columns() is not a
+     * parent method on the Moodle 5.0/5.1 base entity (it was formalised in
+     * 5.2), so the attribute would fatal on those branches.
+     *
+     * @return column[]
+     */
     protected function get_available_columns(): array {
         $campaignsalias = $this->get_table_alias('local_mailwhistle_campaigns');
 
